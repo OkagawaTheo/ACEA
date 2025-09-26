16e01e010228f77deca8187ea5b997bbdd72139f
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-09-26 14:52:58 -0300

    criação de uma classe abstrata para Pessoas e Educacao Por enquanto os métodos estão vazios porém a lógica de views/url será adicionada posteriormente

--- backend/acea_backend/educacao/models.py
+++ backend/acea_backend/educacao/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Educacao(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True,null=True)
+    horario = models.CharField(max_length=50)
+    valor_mensalidade = models.DecimalField(max_digits=10,decimal_places=2)
+
+    class Meta:
+        abstract = True
+
+
+    def __str__(self):
+        return self.nome
+
+class TCurso(Educacao):
+
+    #Relacionamento
+    professor = models.ForeignKey(
+        'pessoas.TProfessor',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='cursos_ministrados' 
+    )
+
+    def criar_curso(self):
+        pass
+        
+    def editar_curso(self):
+        pass
+        
+    def visualizar_curso(self):
+        pass
+        
+    def adicionar_aluno(self):
+        pass
+
+
+class TAtividade_esportiva(Educacao):
+    def criar_atividade_esportiva(self):
+        pass
+
+    professor = models.ForeignKey(
+        'pessoas.TProfessor',
+        on_delete=models.SET_NULL,
+        null = True,
+        related_name='atividades_ministradas'
+    )
+
+    def criar_atividade_esportiva(self):
+        pass
+        
+    def visualizar_atividade_esportiva(self):
+        pass
+
+    def editar_atividade_esportiva():
+        pass
+
+    def adicionar_aluno(self):
+        pass
+
--- backend/acea_backend/pessoas/models.py
+++ backend/acea_backend/pessoas/models.py
@@ -2,60 +2,103 @@ from django.db import models
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=255)
-    cpf = models.CharField(max_length=14,unique=True)
-    phone = models.CharField(max_length=14,blank=True)
+    cpf = models.CharField(max_length=14, unique=True, blank=True, null=True)
+    telefone = models.CharField(max_length=14, blank=True, null=True)
     email = models.EmailField(max_length=255)
 
     class Meta:
         abstract = True
-
+        
+    def __str__(self):
+        return self.nome
 
 
 class TAluno(Pessoa):
-    # adicionar relacionamentos N:N(1)
-    dataNascimento = models.DateField(null=True,blank=True)
+    # add CRUD em services depois de construir a logica
+    data_nascimento = models.DateField(null=True, blank=True)
     endereco = models.CharField(max_length=255)
     
-    def criarAluno(self):
+    def criar_aluno(self):
         pass
 
-    def editarAluno(self):
+    def editar_aluno(self):
         pass
 
-    def excluirAluno(self):
+    def excluir_aluno(self):
         pass
 
-    def visualizarAluno(self):
+    def visualizar_aluno(self):
         pass
 
     def __str__(self):
         return f"Aluno: {self.nome}"
 
+
 class TProfessor(Pessoa):
     especialidade = models.CharField(max_length=100)
 
-    def cadastrarAluno(): #TAluno<curso>: Tcurso,atividade>, TAtividadeEspo.
+    def cadastrar_aluno(self, curso_ou_atividade=None):
         pass
 
-    def visualizarPagamentos(): #list<TPagamentos>
+    def visualizar_pagamentos(self):
         pass
 
-    def gerenciarCurso():
+    def gerenciar_curso(self):
         pass
 
-    def gerenciarAtividadeEsportiva():
+    def gerenciar_atividade_esportiva(self):
         pass
 
-    def criarProfessor():
+    def criar_professor(self):
         pass
 
-    def alterarProfessor():
+    def alterar_professor(self):
         pass
 
-    def excluirProfessor():
+    def excluir_professor(self):
         pass
 
-    def visualizarProfessor():
+    def visualizar_professor(self):
         pass
 
+    def __str__(self):
+        return f"Professor: {self.nome}"
+
+
+class TPresidente(Pessoa):
     
+    def visualizar_pagamento(self):
+        pass
+        
+    def gerenciar_doacoes(self, doacao):
+        pass
+        
+    def controlar_documento(self):
+        pass
+        
+    def download_documento(self):
+        pass
+
+    def criar_presidente(self):
+        pass
+        
+    def alterar_presidente(self):
+        pass
+        
+    def excluir_presidente(self):
+        pass
+        
+    def __str__(self):
+        return f"Presidente: {self.nome}"
+
+
+class TAdm(Pessoa):
+    
+    def gerenciar_professor(self):
+        pass
+
+    def gerenciar_presidente(self):
+        pass
+        
+    def __str__(self):
+        return f"Administrador: {self.nome}"
