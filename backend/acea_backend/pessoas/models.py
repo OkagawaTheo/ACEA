f3e1bfde52d6a606979da67c541eea320a1e0396
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-09-26 14:00:21 -0300

    modelo abstrato de Pessoas

--- backend/acea_backend/pessoas/models.py
+++ backend/acea_backend/pessoas/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14,unique=True)
+    phone = models.CharField(max_length=14,blank=True)
+    email = models.EmailField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+
+class TAluno(Pessoa):
+    # adicionar relacionamentos N:N(1)
+    dataNascimento = models.DateField(null=True,blank=True)
+    endereco = models.CharField(max_length=255)
+    
+    def criarAluno(self):
+        pass
+
+    def editarAluno(self):
+        pass
+
+    def excluirAluno(self):
+        pass
+
+    def visualizarAluno(self):
+        pass
+
+    def __str__(self):
+        return f"Aluno: {self.nome}"
+
+class TProfessor(Pessoa):
+    especialidade = models.CharField(max_length=100)
+
+    def cadastrarAluno(): #TAluno<curso>: Tcurso,atividade>, TAtividadeEspo.
+        pass
+
+    def visualizarPagamentos(): #list<TPagamentos>
+        pass
+
+    def gerenciarCurso():
+        pass
+
+    def gerenciarAtividadeEsportiva():
+        pass
+
+    def criarProfessor():
+        pass
+
+    def alterarProfessor():
+        pass
+
+    def excluirProfessor():
+        pass
+
+    def visualizarProfessor():
+        pass
+
+    
