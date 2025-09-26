267b4811599d92ebf7b081fe6cec1d90a3bc62f6
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-09-26 17:32:15 -0300

    serializers e viewSets para classe Pessoa

--- backend/acea_backend/pessoas/serializers.py
+++ backend/acea_backend/pessoas/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import TAluno
+from .models import *
 
 class TAlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TAluno
         fields = ('id', 'nome', 'cpf', 'telefone', 'email', 'data_nascimento', 'endereco', 'cursos', 'atividades')
+
+class TProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TProfessor
+        fields = ('id','nome','cpf','telefone','email','especialidade','cursos_ministrados','atividades_ministradas')
+
+class TPresidenteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TPresidente
+        fields = ('id','nome','cpf','telefone','email')
+
+        
--- backend/acea_backend/pessoas/views.py
+++ backend/acea_backend/pessoas/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import TAluno
-from .serializers import TAlunoSerializer
+from .models import *
+from .serializers import *
 
-class TAluniViewSet(viewsets.ModelViewSet):
+class TAlunoViewSet(viewsets.ModelViewSet):
     queryset = TAluno.objects.all()
     serializer_class = TAlunoSerializer
+
+class TProfessorViewSet(viewsets.ModelViewSet):
+    queryset = TProfessor.objects.all()
+    serializer_class = TProfessorSerializer
+
+
+class TPresidenteViewSet(viewsets.ModelViewSet):
+    queryset = TPresidente.objects.all()
+    serializer_class = TPresidenteSerializer
