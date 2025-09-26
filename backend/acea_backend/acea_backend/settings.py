535be52acb39aa4ee91c8b714cdcc6dab512da3b
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-09-26 15:44:51 -0300

    criação das tabelas no banco e estrutura de dados inicial feita

--- backend/acea_backend/acea_backend/settings.py
+++ backend/acea_backend/acea_backend/settings.py
@@ -12,15 +12,48 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'pessoas',
+    'educacao',
+    'financeiro',
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware', 
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware', 
+    'django.contrib.messages.middleware.MessageMiddleware', 
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'acea_backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 try:
     from .settings_local import *
 except ImportError:
     pass
 
+
 if 'DATABASES' not in locals():
-    print("Aviso: settings_local.py não encontrado. Usando SQLite padrão.")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -28,6 +61,13 @@ if 'DATABASES' not in locals():
         }
     }
 
-
 if 'SECRET_KEY' not in locals():
     SECRET_KEY = 'chave_de_fallback_insegura_para_dev_se_settings_local_falhar'
+    
+WSGI_APPLICATION = 'acea_backend.wsgi.application'
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- backend/acea_backend/financeiro/models.py
+++ backend/acea_backend/financeiro/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+class TPagamento(models.Model):
+    valor = models.DecimalField(max_digits=10,decimal_places=2)
+    data = models.DateField()
+    tipo = models.CharField(max_length=50) 
+    status = models.CharField(max_length=50) 
+
+    aluno = models.ForeignKey(
+        'pessoas.TAluno',
+        on_delete=models.CASCADE,
+        related_name= 'pagamentos'
+    )
+
+    def criar_pagamento(self):
+        pass
+
+    def editar_pagamento(self):
+        pass
+
+    def visualizar_pagamento(self):
+        pass
+
+    def excluir_pagamento(self):
+        pass
+
+    def __str__(self):
+        return f"Pagamento R$ {self.valor}"
+    
+class TDoacao(models.Model):
+    valor = models.DecimalField(max_digits=10,decimal_places=2)
+    data = models.DateField()
+    doador_info = models.CharField(max_length=255)
+
+    def criar_doacao(self):
+        pass
+
+    def excluir_doacao(self):
+        pass
+
+    def visualizar_doacao(self):
+        pass
+
+    def __str__(self):
+        return f"Doação R$ {self.valor} de {self.doador_info}"
+    
+
+class TDocumentos(models.Model):
+    nome = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=50)
+    data_upload = models.DateTimeField(auto_now_add=True)
+
+    arquivo = models.FileField(upload_to='Documentos/', null=True,blank=True) # Alterar depois o local de upload de arquivo
+
+    def criar_documento(self):
+        pass
+
+    def excluir_documento(self):
+        pass
+
+    def editar_documento(self):
+        pass
+
+    def visualizar_documento(self):
+        pass
+
+    def __str__(self):
+        return self.nome
+    
