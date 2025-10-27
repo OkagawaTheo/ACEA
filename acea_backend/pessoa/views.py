d7437c22de741144b5c9386fa39e6c1decacaf6a
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-10-27 16:26:41 -0300

    test listview

--- acea_backend/pessoa/views.py
+++ acea_backend/pessoa/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView 
+from .models import Aluno
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class AlunoListView(LoginRequiredMixin, ListView):
+    model = Aluno
+    template_name = 'pessoas/aluno_lista.html'
+    context_object_name = 'alunos'
+    
