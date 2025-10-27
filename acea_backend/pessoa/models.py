7201ee780155ed384a2cdbf9c579d2757dcbb391
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-10-27 12:26:45 -0300

    começo models

--- acea_backend/pessoa/models.py
+++ acea_backend/pessoa/models.py
@@ -2,5 +2,26 @@ from django.db import models
 
 class Pessoa(models.Model):
     #nome cpf tel email
-    nome = models.CharField(max_length=100)
-    cpf = models.
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14,unique=True)
+    tel = models.CharField(max_length=15,blank=True,null=True) 
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        abstract = True;
+
+    def __str__(self):
+        return self.nome
+    
+class Aluno(Pessoa):
+    data_nasc = models.DateField()
+    endereco = models.CharField(max_length=400)
+
+    # relations
+
+class Professor(Pessoa):
+    especialidade = models.CharField(max_length=100) # add choices depois (puxar do banco)
+
+
+class Presidente(Pessoa):
+    pass
