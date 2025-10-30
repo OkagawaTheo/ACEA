bce2dc4bbeac5c177b968ba660c29fb53af3807d
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-10-30 14:40:09 -0300

    classe AtividadeEsportiva implementada

--- acea_backend/curso/admin.py
+++ acea_backend/curso/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Curso
+from .models import Curso,AtividadeEsportiva
 
-admin.site.register(Curso)
+admin.site.register(Curso) 
+admin.site.register(AtividadeEsportiva)
+
--- acea_backend/curso/models.py
+++ acea_backend/curso/models.py
@@ -1,24 +1,32 @@
 from django.db import models
-
+from pessoa.models import Professor
 
 class AbstractCurso(models.Model):
-    nome = models.CharField(max_length=255,default=''),
-    id_curso = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255,default='')
 
     class Meta:
         abstract = True
+    def __str__(self):
+        return self.nome
 
 class Curso(AbstractCurso):
+    id_curso = models.AutoField(primary_key=True)
+
     professores = models.ManyToManyField(
         'pessoa.Professor',
         blank=True,
-        related_name='curso_ministrado'
+        related_name='curso_ministrado',
+    )    
+
+    class Meta:
+        db_table = 'Curso'
+
+class AtividadeEsportiva(AbstractCurso):
+    id_ativesportiva = models.AutoField(primary_key=True)
+
+    id_professor = models.ForeignKey(
+        Professor,
+        blank=True,
+        default='',   
     )
-
-    def __str__(self):
-        return self.nome_curso
-    
-
-
-# class AtividadeEsportiva(models.Model):
     
