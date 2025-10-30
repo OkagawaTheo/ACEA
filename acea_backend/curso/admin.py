15107447b38042124d0443b5a1f1095b9c4c2d78
Theo Okagawa <okwath@ccomp3-apc.localdomain>
2025-10-30 17:42:27 -0300

    Model de AtividadeEsportiva implementada

--- acea_backend/curso/admin.py
+++ acea_backend/curso/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Curso,AtividadeEsportiva
+<<<<<<< HEAD
 
 admin.site.register(Curso) 
 admin.site.register(AtividadeEsportiva)
 
+=======
+
+admin.site.register(Curso)
+admin.site.register(AtividadeEsportiva)
+>>>>>>> a66c41c (Model de AtividadeEsportiva implementada)
